9eaf4fd68cff1e645f9824c3bbebbc56e5d78f95
Mika <mikathedev@proton.me>
2023-12-04 20:10:54 -0500

    made input function

--- calculater/src/main.rs
+++ calculater/src/main.rs
@@ -1,3 +1,14 @@
+use std::io::stdin;
+
+fn input(x:String) -> String {
+    println!("{x}");
+    let mut a: String = String::new();
+    stdin().read_line(&mut a).expect("enter something... \n if you did there a was an error 🤷‍♂️🤷‍♂️🤷‍♂️?????????");
+    return a;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let num1: String = input("hello the message is working".to_string());
+    println!("{num1}")
+    
 }
